39f448afc1990ddc58d3b627a4b25c88c1433e51
alena <alena.15243021@gmail.com>
2019-11-17 12:50:37 +0300

    added working slider in main block

---
+++ slider.js
@@ -0,0 +1,13 @@
+$(document).on('ready', function() {
+            $(".lazy").slick({
+                dots: true,
+                infinite: true,
+                autoplay: true,
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                speed: 100,
+                fade: true,
+                cssEase: 'linear'
+
+      });
+    });
