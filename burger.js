f6ceac466bb1702db04af4a4e383b40119647c27
alena <alena.15243021@gmail.com>
2019-11-18 12:33:03 +0300

    improved menu work in the mobile version

--- burger.js
+++ burger.js
@@ -1,13 +1,13 @@
 $(document).on('ready', function() {
-	let burger = $("#burger");
-	console.log(burger);
+	
 
-	$("#burger").click(function(){
-		$("#menu").toggleClass("invisible");
+	$(document).mouseup(function (e){
+	let burger = $("#menu-burger");
+		if(!burger.is(e.target) && burger.has(e.target).length === 0){
+		$("#menu, .open, .close-btn").toggleClass("invisible");
+		}
 	})
+	
 
 
-	$("#menu a").click(function(){
-			$("#menu").toggleClass("invisible");
-		})
 });
